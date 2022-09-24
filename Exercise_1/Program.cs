30f65b164346d4f9c83f2536e8271a0e54c9493c
Nika <nikarasevas@gmail.com>
2022-09-24 16:21:13 +0300

    Проверка 5-значного числа на палиндром

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Программа, которая принимает на вход пятизначное число 
+и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int num1 = number / 10000;
+int num2 = (number / 1000) % 10;
+int num3 = (number / 100) % 10;
+int num4 = (number / 10) % 10;
+int num5 = number % 10;
+
+if ((num1 == num5) && (num2 == num4))
+    {
+        Console.WriteLine($"Заданное число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Заданное число {number} НЕ является палиндромом");
+    }
+
