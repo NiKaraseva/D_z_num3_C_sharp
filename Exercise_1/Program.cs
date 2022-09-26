3bd1139338c620f96c76ce0d383d9182ffeabe0c
Nika <nikarasevas@gmail.com>
2022-09-27 02:15:04 +0300

    Проверяет на палиндром (2-мя способами: int и string)

--- Exercise_1/Program.cs
+++ Exercise_1/Program.cs
@@ -4,8 +4,10 @@
 12821 -> да
 23432 -> да*/
 
-Console.WriteLine("Введите пятизначное число: ");
-int number = int.Parse(Console.ReadLine());
+// (!) ВАРИАНТ 1
+
+/* Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
 
 int num1 = number / 10000;
 int num2 = (number / 1000) % 10;
@@ -13,7 +15,34 @@ int num3 = (number / 100) % 10;
 int num4 = (number / 10) % 10;
 int num5 = number % 10;
 
-if ((num1 == num5) && (num2 == num4))
+if ((number >= 10000) && (number < 100000))
+{
+    if ((num1 == num5) && (num2 == num4))
+    {
+        Console.WriteLine($"Заданное число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Заданное число {number} НЕ является палиндромом");
+    } 
+
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число, повторите ввод!");
+}*/ 
+
+
+// (!) ВАРИАНТ 2
+
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+
+int length = number.Length;
+
+if (length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
     {
         Console.WriteLine($"Заданное число {number} является палиндромом");
     }
@@ -21,4 +50,8 @@ if ((num1 == num5) && (num2 == num4))
     {
         Console.WriteLine($"Заданное число {number} НЕ является палиндромом");
     }
-
+}
+else
+{
+    Console.WriteLine("Вы ввели не пятизначное число, повторите ввод!");
+}
